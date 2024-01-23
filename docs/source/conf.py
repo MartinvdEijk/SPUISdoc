2089f4e9e7b91a0d1d4af911f7585b70587cfc88
martin-eijk <martinvandereijk1995@gmail.com>
2024-01-23 15:25:05 +0100

    Last submit of Readme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'SPUIS'
+copyright = '2023, Eijk van der'
+author = 'Martin van der Eijk'
 
 release = '0.1'
 version = '0.1.0'
